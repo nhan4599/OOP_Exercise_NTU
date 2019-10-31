bd7e504d70089067f8679614f964c1c4a851d840
hieu <nhokhieuthao>
2019-10-31 13:12:22 +0700

    Doing part 2

---
+++ OOP_Exercise_NTU_All/OOP_Exercise_NTU_Part2/Point.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace OOP_Exercise_NTU_Part2
+{
+    class Point
+    {
+        private int x, y;
+
+        public Point()
+        {
+            this.x = this.y = 0;
+        }
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int GetX()
+        {
+            return this.x;
+        }
+
+        public void SetX(int x)
+        {
+            this.x = x;
+        }
+
+        public int GetY()
+        {
+            return this.y;
+        }
+
+        public void SetY(int y)
+        {
+            this.y = y;
+        }
+
+        public int[] GetXY()
+        {
+            int[] rs = new int[2];
+            rs[0] = this.x;
+            rs[1] = this.y;
+            return rs;
+        }
+
+        public void SetXY(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", this.x, this.y);
+        }
+    }
+}
